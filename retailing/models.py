4ced522f8c75166d8a8366f61ecccef578e52aec
vyacheslavsafin <safin.vr@mail.ru>
2024-01-29 14:00:25 +0300

    правка модели и добавление команды на создание суперпользователя

--- retailing/models.py
+++ retailing/models.py
@@ -10,6 +10,10 @@ class Dealer(models.Model):
     city = models.CharField(max_length=50, verbose_name='город')
     street = models.CharField(max_length=50, verbose_name='улица')
     house = models.CharField(max_length=50, verbose_name='дом')
+    supplier = models.ForeignKey('self', verbose_name='Поставщик', on_delete=models.SET_NULL, **NULLABLE)
+    debt = models.IntegerField(default=0, verbose_name='Долг поставщику')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
 
     def __str__(self):
         return f'{self.title}'
