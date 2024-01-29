6e5563b524712056f88e92886b1708884d77b536
vyacheslavsafin <safin.vr@mail.ru>
2024-01-29 15:11:53 +0300

    добавлена возможность погасить задолженность через admin action

--- retailing/admin.py
+++ retailing/admin.py
@@ -8,6 +8,11 @@ class DealerAdmin(admin.ModelAdmin):
     readonly_fields = ('level',)
     list_display = ('title', 'supplier', 'debt', 'date_created')
     list_filter = ('city',)
+    actions = ['clean_debt']
 
+    def clean_debt(self, request, queryset):
+        queryset.update(debt=0)
+
+    clean_debt.short_description = 'Погасить задолженность'
 
 admin.site.register(Product)
